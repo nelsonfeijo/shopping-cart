a8191bbe02c4ed5da2cf85e1b83e0d8acae4cdf1
Nelson Feijó <114948173+nelsonfeijo@users.noreply.github.com>
2024-05-12 22:13:11 -0300

    Att

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import './Header.css'
 import CartButton from "../CartButton/CartButton";
+import { FaHandshakeSlash } from "react-icons/fa6";
 
 function Header (){
   return (
     <header className="header">
       <div className="container">
+        <span className="title"><FaHandshakeSlash />Mercado Preso</span>
       <SearchBar />
       <CartButton />
       </div>
